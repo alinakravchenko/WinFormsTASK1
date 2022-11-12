120d3f7d8e7f40d30aedb1cdb32ada2c94a7b75f
ms.alievaalya@gmail.com <ms.alievaalya@gmail.com>
2022-11-12 13:26:03 +0300

    Add project files.

---
+++ WinFormsTASK1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WinFormsTASK1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ShowMesBox = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ShowMesBox
+            // 
+            this.ShowMesBox.Location = new System.Drawing.Point(293, 189);
+            this.ShowMesBox.Name = "ShowMesBox";
+            this.ShowMesBox.Size = new System.Drawing.Size(132, 23);
+            this.ShowMesBox.TabIndex = 0;
+            this.ShowMesBox.Text = "ShowMesBox";
+            this.ShowMesBox.UseVisualStyleBackColor = true;
+            this.ShowMesBox.Click += new System.EventHandler(this.ShowMesBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ShowMesBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button ShowMesBox;
+    }
+}
---
+++ WinFormsTASK1/Form1.cs
@@ -0,0 +1,30 @@
+﻿namespace WinFormsTASK1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Shown += ShowMesBox_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowMesBox_Click(object sender, EventArgs e)
+        {
+            string[] array = { "Student: Кравченко Алина", "Предмет: Windows Forms", "Группа: ПВ111" };
+            int numSymbol = 0;
+            string caption;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                numSymbol += array[i].Length;
+                caption = (array.Length - 1 == i) ? $"MessageBox {i + 1}. Среднее количество символов - {numSymbol / array.Length}" : $"MessageBox {i + 1}";
+                MessageBox.Show(array[i], caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
